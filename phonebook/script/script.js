ac097dc2c17ea67abafecd6618a3c5e3ba988666
Павел Бакшеев <sxeiter@mail.ru>
2023-11-13 17:57:55 +0500

    дз 6

--- phonebook/script/script.js
+++ phonebook/script/script.js
@@ -201,6 +201,7 @@ const data = [
       list: table.tbody,
       logo,
       btnAdd: buttonGroup.btns[0],
+      btnDel: buttonGroup.btns[1],
       formOverlay: form.overlay,
       form: form.form,
     };
@@ -208,6 +209,7 @@ const data = [
 
   const createRow = ({name: firstName, surname, phone}) => {
     const tr = document.createElement('tr');
+    tr.classList.add('contact');
 
     const tdDel = document.createElement('td');
     tdDel.classList.add('delete');
@@ -256,7 +258,7 @@ const data = [
     const app = document.querySelector(selectorApp);
     const phoneBook = renderPhoneBook(app, title);
 
-    const {list, logo, btnAdd, formOverlay, form} = phoneBook;
+    const {list, logo, btnAdd, formOverlay, form, btnDel} = phoneBook;
     // функционал
     const allRow = renderContacts(list, data);
 
@@ -266,12 +268,25 @@ const data = [
       formOverlay.classList.add('is-visible');
     });
 
-    form.addEventListener('click', e => {
-      e.stopImmediatePropagation();
+    formOverlay.addEventListener('click', e => {
+      const target = e.target;
+      if (target === formOverlay ||
+        target.closest('.close')) {
+        formOverlay.classList.remove('is-visible');
+      }
     });
 
-    formOverlay.addEventListener('click', () => {
-      formOverlay.classList.remove('is-visible');
+    btnDel.addEventListener('click', () => {
+      document.querySelectorAll('.delete').forEach(del => {
+        del.classList.toggle('is-visible');
+      });
+
+      list.addEventListener('click', e => {
+        const target = e.target;
+        if (target.closest('.del-icon')) {
+          target.closest('.contact').remove();
+        }
+      });
     });
   };
 
